000cf4bf3c11371d321c8bada5fe183f865910f2
mubarakismail <mubarakismail202@gmail.com>
2022-09-09 23:20:38 +0200

    Fix bugs from first submission

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const apiKey = "cdee76943066d53839424f2365c08769";
+const apiKey = "cdee76943066d53839424f2365c08769&units=metric";
 const api_point = "https://api.openweathermap.org/data/2.5/weather?zip=";
 //{zip code},{country code}&appid={API key}
 
@@ -15,11 +15,13 @@ document
 function addInfoAndReturnIt() {
   const newZipCode = document.getElementById("zip").value;
   const feelingsOfWeather = document.getElementById("feelings").value;
-  getInfo("/getWeather").then(async function () {
-    const dataFromPost = await postWeatherFeelings("/addFeelings", {
+  getInfo(api_point + newZipCode + ",us&appid=" + apiKey).then(function (data) {
+    postWeatherFeelings("/addFeelings", {
       feelings: feelingsOfWeather,
+      date: newDate,
+      temperature: data.main.temp
     });
-    updateUI(api_point + newZipCode + ",us&appid=" + apiKey, dataFromPost);
+    updateUI("/allData");
   });
 }
 
@@ -27,7 +29,6 @@ const getInfo = async (url) => {
   const res = await fetch(url);
   try {
     const data = await res.json();
-    /* console.log(data); */
     return data;
   } catch (error) {
     console.log("error" + error);
@@ -52,15 +53,16 @@ const postWeatherFeelings = async (url = "", data = {}) => {
   }
 };
 
-const updateUI = async (url = "", data) => {
+const updateUI = async (url = "") => {
   const request = await fetch(url);
   try {
     // Transform into JSON
     const allData = await request.json();
+    console.log(allData);
     document.getElementById("date").innerHTML = newDate;
     document.getElementById("temp").innerHTML =
-      (allData.main.temp - 273.15).toFixed(2) + "&deg; C";
-    document.getElementById("content").innerHTML = data.feelings;
+      allData.temperature + "&deg; C";
+    document.getElementById("content").innerHTML = allData.feelings;
   } catch (error) {
     console.log("error", error);
     // appropriately handle the error
